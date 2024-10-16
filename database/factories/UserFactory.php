8316cbf00adf0be95c45693431950c9f2b936620
Gabriel Alves <gabriel.alves450@hotmail.com>
2024-10-16 14:55:16 -0300

    Created user and project models

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'rating' => fake()->randomElement([1, 2, 3, 4, 5]),
+            'avatar' => 'https://avatar.iran.liara.run/public'
         ];
     }
 
@@ -37,7 +36,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
